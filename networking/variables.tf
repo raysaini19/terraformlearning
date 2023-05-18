998d039dc92e85a2ea4631f39db1218da44f73c3
manoj-saini-nbs <82801297+manoj-saini-nbs@users.noreply.github.com>
2023-05-18 22:09:19 +0100

    delete vpc

--- networking/variables.tf
+++ networking/variables.tf
@@ -5,29 +5,29 @@ variable "aws_region" {
 }
 
 
-variable "vpc_name" {
-  description = "VPC Name"
-  type        = string
-  #default = "my-vpc"
-}
+# variable "vpc_name" {
+#   description = "VPC Name"
+#   type        = string
+#   #default = "my-vpc"
+# }
 
-# VPC CIDR Block
-variable "vpc_cidr_block" {
-  description = "VPC CIDR Block"
-  type        = string
-  #default     = "10.14.0.0/16"
-}
+# # VPC CIDR Block
+# variable "vpc_cidr_block" {
+#   description = "VPC CIDR Block"
+#   type        = string
+#   #default     = "10.14.0.0/16"
+# }
 
-# VPC Availability Zones
-variable "vpc_availability_zones" {
-  description = "VPC Availability Zones"
-  type        = list(string)
-  #default     = ["ap-south-1a", "ap-south-1b"]
-}
+# # VPC Availability Zones
+# variable "vpc_availability_zones" {
+#   description = "VPC Availability Zones"
+#   type        = list(string)
+#   #default     = ["ap-south-1a", "ap-south-1b"]
+# }
 
-# VPC Public Subnets
-variable "vpc_public_subnets" {
-  description = "VPC Public Subnets"
-  type        = list(string)
-  #default     = ["10.14.101.0/24", "10.14.102.0/24"]
-}
+# # VPC Public Subnets
+# variable "vpc_public_subnets" {
+#   description = "VPC Public Subnets"
+#   type        = list(string)
+#   #default     = ["10.14.101.0/24", "10.14.102.0/24"]
+# }
